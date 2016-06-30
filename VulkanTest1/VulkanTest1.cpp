0772f1b02ab1a849c5735c81c8742442a9d0aa3d
Aaron Hull <Aaron Hull>
2016-06-30 01:42:17 -0400

    Created instance sample

--- VulkanTest1/VulkanTest1.cpp
+++ VulkanTest1/VulkanTest1.cpp
@@ -1,2 +1,33 @@
 #include <vulkan\vulkan.h>
 
+int main(int argc, char *argv[])
+{
+  // Define App Info for VK.
+  VkApplicationInfo vkAppInfo = {};
+  vkAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  vkAppInfo.pNext = NULL;
+  vkAppInfo.pApplicationName = "VulkanTest";
+  vkAppInfo.applicationVersion = 0;
+  vkAppInfo.pEngineName = NULL;
+  vkAppInfo.engineVersion = 0;
+  vkAppInfo.apiVersion = VK_API_VERSION_1_0;
+
+  // Define Vk Info.
+  VkInstanceCreateInfo vkInstanceInfo = {};
+  vkInstanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  vkInstanceInfo.pApplicationInfo = &vkAppInfo;
+  vkInstanceInfo.pNext = NULL;
+  vkInstanceInfo.ppEnabledExtensionNames = NULL;
+  vkInstanceInfo.enabledExtensionCount = 0;
+  vkInstanceInfo.ppEnabledLayerNames = NULL;
+  vkInstanceInfo.enabledLayerCount = 0;
+  vkInstanceInfo.flags = 0;
+
+  VkInstance vkInstance;
+  VkResult vkResult;
+
+  vkResult = vkCreateInstance(&vkInstanceInfo, NULL, &vkInstance);
+  vkDestroyInstance(vkInstance, NULL);
+
+  return 0;
+}
